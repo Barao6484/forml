ef236fdee729e7518614c821e8419c40b14fab2b
Barao6484 <151473886+Barao6484@users.noreply.github.com>
2024-01-31 21:34:11 +0000

    atualização 31-01-2024
    enviei as os arquivos para pasta dentro do diretório

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from "./img/my.jpg";
 import './App.css';
 
-function App() {
+export default function App() {
+  const [formData, setFormData] = useState({
+    Name: '',
+    Email: '',
+    phone: '',
+    Subject: '',
+    Message: ''
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const { Name, Email, phone, Subject, Message } = formData;
+
+    try {
+      const response = await fetch("https://api.sheetmonkey.io/form/dZxS4swjd2knDULrmd1hf2", {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ Name, Email, phone, Subject, Message })
+      });
+
+      if (response.ok) {
+        alert('Dados Submetidos com sucesso');
+        setFormData({
+          Name: '',
+          Email: '',
+          phone: '',
+          Subject: '',
+          Message: ''
+        });
+      } else {
+        throw new Error('Erro ao submeter os dados');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('Erro ao submeter os dados');
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Formulario de contacto</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder='Nome'
+          name='Name'
+          type='text'
+          value={formData.Name}
+          onChange={handleChange}
+        />
+        <input
+          placeholder='Email'
+          name='Email'
+          type='email'
+          value={formData.Email}
+          onChange={handleChange}
+        />
+        <input
+          placeholder='Telefone'
+          name='phone'
+          type='tel'
+          value={formData.phone}
+          onChange={handleChange}
+        />
+        <input
+          placeholder='Assunto'
+          name='Subject'
+          type='text'
+          value={formData.Subject}
+          onChange={handleChange}
+        />
+        <textarea
+          placeholder='Mensagem'
+          name='Message'
+          value={formData.Message}
+          onChange={handleChange}
+        ></textarea>
+        <input className="button" type="submit" />
+      </form>
     </div>
   );
 }
-
-export default App;
